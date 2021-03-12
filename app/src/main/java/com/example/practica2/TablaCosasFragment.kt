89edb76266b398fba4a91b03529494e3eace8235
Diego Ms <diego.mtzs00@gmail.com>
2021-03-11 19:34:01 -0600

    Otra actualizacion

--- app/src/main/java/com/example/practica2/TablaCosasFragment.kt
+++ app/src/main/java/com/example/practica2/TablaCosasFragment.kt
@@ -8,9 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import android.widget.TextView
-//import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
